0d44c718b5995d9010d69e02f958d13024fb5a83
jeonjaekyun <jkyun01@gmail.com>
2020-06-25 17:27:42 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
+const config = require('./config/key');
 const app = express();
 const port = 5000;
 
-mongoose.connect('mongodb+srv://jaekyun:abcd1234@boilerplate-ouq7w.mongodb.net/<dbname>?retryWrites=true&w=majority',
+mongoose.connect(config.mongoURI,
 {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false})
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
-app.get('/', (req,res) => res.send('Hello World!'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => res.send('Hello World!!'));
+
+app.post('/register', (req,res) => {
+    const user = new User(req.body);
+
+    user.save((err,userInfo) => {
+        if(err) return res.json({success:false,err});
+        return res.status(200).json({
+            success:true
+        });
+    });
+});
 
 app.listen(port, () => console.log(`port : ${port}`));
